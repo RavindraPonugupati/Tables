6fa9fab86db76bd9f696962227fe1fe7085f7d83
RavindraPonugupati <46193873+RavindraPonugupati@users.noreply.github.com>
2019-01-23 00:07:16 +0530

    Add files via upload

---
+++ src/services/fakeMovieService.jsx
@@ -0,0 +1,52 @@
+//import * as genresAPI from './fakeGenreService';
+
+
+const movies = [
+    {
+        _id: "522fdfZcxzcedfzcszcfewfrf",
+        title: "Terminator",
+        genre: { _id: "fdsfsdfdsfsdfddfsdsfdsf", name: "Action"},
+        numberInStock: 6,
+        dailyRentalRate: 2.5,
+        publishDate: "2018-01-03T19:04:28.809Z"
+    },
+    {
+        _id: "dsfrefdsfsdfsre654321682",
+        title: "Terminator",
+        genre: { _id: "dsfsfdsfdsrewrgrtyrtfghjhyj", name: "Action"},
+        numberInStock: 7,
+        dailyRentalRate: 5.5,
+        publishDate: "2018-01-03T19:04:28.809Z"
+    },
+    {
+        _id: "defds2568413rewrewrewrv",
+        title: "Die Hard",
+        genre: { _id: "trgfgdfg564354846erewre", name: "Action"},
+        numberInStock: 8,
+        dailyRentalRate: 9.5,
+        publishDate: "2018-01-03T19:04:28.809Z"
+    },
+    {
+        _id: "qwqwqet56456trjui89hfggfg",
+        title: "Die",
+        genre: { _id: "fgtytryhji54354767ugdfgdfgt", name: "Action"},
+        numberInStock: 9,
+        dailyRentalRate: 8.6,
+        publishDate: "2018-01-03T19:04:28.809Z"
+    },
+    {
+        _id: "5435hty65yhgjkuljhtry",
+        title: "Airplane",
+        genre: { _id: "reerewrwctert6546565", name: "Action"},
+        numberInStock: 10,
+        dailyRentalRate: 10.2,
+        publishDate: "2018-01-03T19:04:28.809Z"
+    }
+];
+    export function getMovies() {
+        return movies;
+
+    }
+
+   
+
